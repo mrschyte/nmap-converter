7c1b27bfd60218f9ffb78f6624124fc837f5d03b
Peter Kasza <peter.kasza@itinsight.hu>
2016-12-13 21:18:03 +0100

    Added confidence and detection method columns; added n/a to flag

--- nmap-converter.py
+++ nmap-converter.py
@@ -11,13 +11,15 @@ def main(report, workbook):
     summary = workbook.add_worksheet("Summary")
     results = workbook.add_worksheet("Results")
 
-    bold = workbook.add_format({"bold": True})
+    fmt_bold = workbook.add_format({"bold": True})
+    fmt_conf = workbook.add_format()
+    fmt_conf.set_num_format('0%')
 
-    results.autofilter("A1:L1")
+    results.autofilter("A1:N1")
     results.freeze_panes(1, 0)
 
     results.data_validation("K2:K$1048576", {"validate": "list",
-                                             "source": ["Y", "N"]})
+                                             "source": ["Y", "N", "N/A"]})
 
     summary_header = ["Command", "Version", "Scan Type", "Started", "Completed", "Hosts Total", "Hosts Up", "Hosts Down"]
     summary_body = {"Command": lambda report: report.commandline,
@@ -29,37 +31,40 @@ def main(report, workbook):
                     "Hosts Up": lambda report: report.hosts_up,
                     "Hosts Down": lambda report: report.hosts_down}
 
-    results_header = ["Host", "IP", "Port", "Protocol", "Status", "Service", "Reason", "Product", "Version", "Extra", "Flagged", "Notes"]
-    results_body = {"Host": lambda host, port, service: head(host.hostnames),
-                    "IP": lambda host, port, service: host.address,
-                    "Port": lambda host, port, service: port[0],
-                    "Protocol": lambda host, port, service: port[1],
-                    "Status": lambda host, port, service: service.state,
-                    "Service": lambda host, port, service: service.service,
-                    "Reason": lambda host, port, service: service.reason,
-                    "Host": lambda host, port, service: head(host.hostnames),
-                    "Product": lambda host, port, service: service.service_dict.get("product", ""),
-                    "Version": lambda host, port, service: service.service_dict.get("version", ""),
-                    "Extra": lambda host, port, service: service.service_dict.get("extrainfo", ""),
-                    "Flagged": lambda host, port, service: "N",
-                    "Notes": lambda host, port, service: ""}
+    results_header = ["Host", "IP", "Port", "Protocol", "Status", "Service", "Method", "Confidence", "Reason", "Product", "Version", "Extra", "Flagged", "Notes"]
+    results_body = {"Host": lambda host, service: head(host.hostnames),
+                    "IP": lambda host, service: host.address,
+                    "Port": lambda host, service: service.port,
+                    "Protocol": lambda host, service: service.protocol,
+                    "Status": lambda host, service: service.state,
+                    "Service": lambda host, service: service.service,
+                    "Method": lambda host, service: service.service_dict.get("method", ""),
+                    "Confidence": lambda host, service: float(service.service_dict.get("conf", "0")) / 10,
+                    "Reason": lambda host, service: service.reason,
+                    "Host": lambda host, service: head(host.hostnames),
+                    "Product": lambda host, service: service.service_dict.get("product", ""),
+                    "Version": lambda host, service: service.service_dict.get("version", ""),
+                    "Extra": lambda host, service: service.service_dict.get("extrainfo", ""),
+                    "Flagged": lambda host, service: "N/A",
+                    "Notes": lambda host, service: ""}
+
+    results_format = {"Confidence": fmt_conf}
 
     print("[+] Processing {}".format(report.summary))
     for idx, item in enumerate(summary_header):
-        summary.write(idx, 0, item, bold)
+        summary.write(idx, 0, item, fmt_bold)
         for idx, item in enumerate(summary_header):
             summary.write(idx, 1, summary_body[item](report))
 
     for idx, item in enumerate(results_header):
-        results.write(0, idx, item, bold)
+        results.write(0, idx, item, fmt_bold)
 
     row = 1
     for host in report.hosts:
         print("[+] Processing {}".format(host))
-        for port in host.get_ports():
+        for service in host.services:
             for idx, item in enumerate(results_header):
-                service = host.get_service(port[0], port[1])
-                results.write(row, idx, results_body[item](host, port, service))
+                results.write(row, idx, results_body[item](host, service), results_format.get(item, None))
             row += 1
                 
     workbook.close()
