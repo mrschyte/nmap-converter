704c775ee1ed8cf1a9b81787d9caf0e94393ba15
Peter Kasza <peter.kasza@itinsight.hu>
2017-11-21 16:43:11 +0100

    Include scripts output as comment in the results sheet.

--- nmap-converter.py
+++ nmap-converter.py
@@ -5,11 +5,20 @@ from xlsxwriter import Workbook
 from datetime import datetime
 
 import os.path
+import pprint
 
 def head(xs, default=""):
     if len(xs) > 0:
         return xs[0]
     return default
+
+def format_scripts_output(results):
+    output = {}
+    for result in results:
+        output[result['id']] = (result['output'], result['elements'])
+    if len(output.keys()) > 0:
+        return pprint.pformat(output)
+    return None
 
 def main(reports, workbook):
     summary = workbook.add_worksheet("Summary")
@@ -58,6 +67,9 @@ def main(reports, workbook):
 
         results_format = {"Confidence": fmt_conf}
 
+        comments = {"Service": lambda host, service: format_scripts_output(service.scripts_results)}
+        comments_format = {"Service": {"width": 500}}
+
         print("[+] Processing {}".format(report.summary))
         for idx, item in enumerate(summary_header):
             summary.write(0, idx, item, fmt_bold)
@@ -72,6 +84,10 @@ def main(reports, workbook):
             for service in host.services:
                 for idx, item in enumerate(results_header):
                     results.write(row, idx, results_body[item](host, service), results_format.get(item, None))
+                    if item in comments:
+                        comment = comments[item](host, service)
+                        if comment != None:
+                            results.write_comment(row, idx, comment, comments_format.get(item, None))
                 row += 1
 
     workbook.close()
